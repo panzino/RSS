acba7038c02ba56898fec00db4175f4a140969d3
Panzino <daniel.nara@outlook.com>
2018-06-18 19:06:09 -0400

    install script is done

--- install.sh
+++ install.sh
@@ -1,25 +1,29 @@
 #Install curl
-apt-get install autoconf automake libtool
-git clone https://github.com/curl/curl.git /home/$(logname)/curl
-cd ..
-cd curl
-./buildconf
-./configure
-make
-make install
+#apt-get install autoconf automake libtool make cmake gcc g++
+#git clone https://github.com/curl/curl.git /home/$(logname)/curl
+#cd ..
+#cd curl
+#./buildconf
+#./configure
+#make
+#make install
+#cd ..
 
 #Install Tidy
-git clone https://github.com/htacg/tidy-html5.git /home/$(logname)/tidy
-cd tidy
-cd build/cmake
-cmake ../.. -DCMAKE_BUILD_TYPE=Release
-make
-sudo make install
+#git clone https://github.com/htacg/tidy-html5.git /home/$(logname)/tidy
+#cd tidy
+#cd build/cmake
+#cmake ../.. -DCMAKE_BUILD_TYPE=Release
+#make
+#sudo make install
 
 #Install rapidxml
-apt-get install librapidxml-dev
+#apt-get install librapidxml-dev
+#cd /usr/include/rapidxml
+#mv rapid* /usr/local/include
 
 #Make the source code
+#DONE
 cd ..
 cd ..
 cd ..
@@ -30,6 +34,6 @@ make
 sudo apt-get install cron
 cd ..
 mv run.sh /etc/cron.hourly
-cd ..
-mkfile crontab
-echo "$[RANDOM%59] * * * * /etc/cron.hourly/run.sh" >> cron.hourly
+cd /etc
+echo -n $(shuf -i 0-60 -n 1) >> crontab
+echo " * * * *" $(logname)  "/etc/cron.hourly/run.sh" >> crontab
